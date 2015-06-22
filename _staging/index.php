e795ae5e99028bf76296a247970c00638f1121f0
Noah <nwsd02@gmail.com>
2015-06-21 17:28:04 -0700

    code formatting

--- _staging/index.php
+++ _staging/index.php
@@ -1,23 +1,23 @@
 <!doctype html>
 <html class="no-js" lang="en">
-<head>
-	<title>The Westwind Project</title>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="description" content="">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<head>
+		<title>The Westwind Project</title>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="description" content="">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<link href="css/styles.css" rel="stylesheet">
-</head>
+		<link href="css/styles.css" rel="stylesheet">
+	</head>
 	<body>
 
 
-<div class="hero">
-	<h1>Hero</h1>
-	<video>
-		<source src="">
-	</video>
-</div>
+	<div class="hero">
+		<h1>Hero</h1>
+		<video>
+			<source src="">
+		</video>
+	</div>
 
 
 		<footer>
--- includes/head.php
+++ includes/head.php
@@ -1,12 +1,12 @@
 <!doctype html>
 <html class="no-js" lang="en">
-<head>
-	<title>The Westwind Project</title>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="description" content="">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<head>
+		<title>The Westwind Project</title>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="description" content="">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<link href="css/styles.css" rel="stylesheet">
-</head>
+		<link href="css/styles.css" rel="stylesheet">
+	</head>
 	<body>
